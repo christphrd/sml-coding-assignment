daff84e3eb41c2cf9cc74213aa54bdabc9e8badf
christphrd <diep.christopher@gmail.com>
2019-03-02 19:41:57 -0500

    Comments for explaining coinCombo function

--- assets/js/coinCombo.js
+++ assets/js/coinCombo.js
@@ -1,18 +1,21 @@
 function coinCombo(coinValues, total, index = 0, combinations = {}, extraCombination = null) {
-    //1. Add combination to list
+    //2. Add extra coin combination to list
     if (extraCombination !== null) {
         combinations = { ...combinations, ...extraCombination }
     }
 
-    
+    //3. If coins add up to total or on the last/lowest coin
     if (total === 0 || (index + 1) === coinValues.length) {
         if ((index + 1) === coinValues.length && total > 0) {
+            //3a. Let the last coin finish off the rest
             let key = coinValues[index], newCombo = {}
             newCombo[key] = total
             combinations = { ...combinations, ...newCombo }
             index += 1
         }
+        //a OR b
         while (index < coinValues.length) {
+            //3b. Finish off the remaining coins. The earlier coins added up to the total. The rest are zero.
             let key = coinValues[index], newCombo = {}
             newCombo[key] = 0
             combinations = { ...combinations, ...newCombo }
@@ -20,12 +23,14 @@ function coinCombo(coinValues, total, index = 0, combinations = {}, extraCombina
             index += 1
         }
         console.log(coinValues.map(coinValue => combinations[coinValue]).join(" | "))
-        return 1
+        return 1 //For keeping track of total count in section 1.
     }
 
+    //1. Iterate through each coin. Each coin goes to its max value that does not exceed the total
     let current = coinValues[index];
     let comboCount = 0;
     for (let i = 0; i < (Math.floor(total / current) + 1); i++) {
+        //1a. Keep track of the coin on this iteration e.g. {value: how many/index}
         let key = coinValues[index], extra = {}
         extra[key] = i
         comboCount += coinCombo(coinValues ,total - (i * current), index + 1, combinations, extra)
@@ -36,7 +41,7 @@ function coinCombo(coinValues, total, index = 0, combinations = {}, extraCombina
 function countCombo(str) {
     let total = parseStrForTotal(str)
     let coinNames = parseStrForCoinNames(str, total) //e.g. { 25: "Quarter", 10: "Dime", 5: "Nickel", 1: "Penny" }
-    let coinValues = Object.keys(coinNames).sort((a,b) => b - a)
+    let coinValues = Object.keys(coinNames).sort((a,b) => b - a) // [25,10,5,1]
 
     console.log(coinValues.map(value => coinNames[value]).join(` | `))
 
@@ -65,13 +70,11 @@ function parseStrForCoinNames(str, total) {
     }
     
     let coinValues = numbers.map(e => total/e)
-    //console.log(coinValues)
     
     let names = [];
     for (let i = 0; i < strArr.length; i += 2) {
         names.push(strArr[i])
     }
-    //console.log(names)
     
     let hash = {};
     for (let i = 0; i < names.length; i++) {
